c83503d53a7407c0583808815b035619068ec786
Loretta Cain <locacodes@gmail.com>
2016-02-18 22:17:00 +1100

    arrows and color changes

--- js/main.js
+++ js/main.js
@@ -1,52 +1,5 @@
 
 
-
-  // Project Images / links
-
-  var hippo = function(){
-    $('.image-popup-vertical-fit').magnificPopup({
-      type: 'image',
-      closeOnContentClick: true,
-      mainClass: 'mfp-img-mobile',
-      image: {
-        verticalFit: true
-      }
-      
-    });
-  };
-
-  var lizard = function(){
-    $('.image-popup-fit-width').magnificPopup({
-        type: 'image',
-        closeOnContentClick: true,
-        image: {
-          verticalFit: false
-        }
-      });
-    }
-
-  var cows = function(){
-    $('.image-popup-no-margins').magnificPopup({
-      type: 'image',
-      closeOnContentClick: true,
-      closeBtnInside: false,
-      fixedContentPos: true,
-      mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-      image: {
-        verticalFit: true
-      },
-      zoom: {
-        enabled: true,
-        duration: 300 // don't foget to change the duration also in CSS
-      }
-    });
-  }
-
-  // Resume Pop up
-
-
-
-
   // Contact and About Me Pop up
 
   var contact = function(){
@@ -70,3 +23,8 @@
  var shark = function(){
   $('#shark').append('<img src="/assets/sharkfin2.png" class="shark2">');
  }
+
+
+setTimeout(function(){
+    $('#arrowkeys').addClass('hiding');
+}, 3000);
